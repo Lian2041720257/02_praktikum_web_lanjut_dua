3cb571c896d60cb163072632822c0d15daa9c6d5
= <=>
2023-02-21 17:00:11 +0700

    Minggu 2

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index(){
+        return "Selamat Datang";
+    }
+
+    public function about(){
+        return "2041720257 Lian Mega Pratiwi";
+    }
+}
